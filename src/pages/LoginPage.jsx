216407289321a6720d7a2a07d152ee0910eabb3d
IraMira333 <irinat333@gmail.com>
2023-08-10 00:13:05 +0300

    Update LoginPage.jsx

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,9 +1,45 @@
+import { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { authOperations } from 'reduxThunk/auth';
+
+const styles = {
+  form: {
+    width: 320,
+  },
+  label: {
+    display: 'flex',
+    flexDirection: 'column',
+    marginBottom: 15,
+  },
+};
+
 export default function LoginPage() {
+  const dispatch = useDispatch();
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleChange = ({ target: { name, value } }) => {
+    switch (name) {
+      case 'email':
+        return setEmail(value);
+      case 'password':
+        return setPassword(value);
+      default:
+        return;
+    }
+  };
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    dispatch(authOperations.logIn({ email, password }));
+    setEmail('');
+    setPassword('');
+  };
   return (
     <div>
       <h1>Страница логина</h1>
 
-      {/* <form onSubmit={handleSubmit} style={styles.form} autoComplete="off">
+      <form onSubmit={handleSubmit} style={styles.form} autoComplete="off">
         <label style={styles.label}>
           Почта
           <input
@@ -25,7 +61,7 @@ export default function LoginPage() {
         </label>
 
         <button type="submit">Войти</button>
-      </form> */}
+      </form>
     </div>
   );
 }
