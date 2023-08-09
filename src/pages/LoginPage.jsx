326f2bed7773e02f447903c21c7b5873267b43d9
IraMira333 <irinat333@gmail.com>
2023-08-09 15:51:05 +0300

    Create LoginPage.jsx

---
+++ src/pages/LoginPage.jsx
@@ -0,0 +1,31 @@
+export default function LoginPage() {
+  return (
+    <div>
+      <h1>Страница логина</h1>
+
+      {/* <form onSubmit={handleSubmit} style={styles.form} autoComplete="off">
+        <label style={styles.label}>
+          Почта
+          <input
+            type="email"
+            name="email"
+            value={email}
+            onChange={handleChange}
+          />
+        </label>
+
+        <label style={styles.label}>
+          Пароль
+          <input
+            type="password"
+            name="password"
+            value={password}
+            onChange={handleChange}
+          />
+        </label>
+
+        <button type="submit">Войти</button>
+      </form> */}
+    </div>
+  );
+}
