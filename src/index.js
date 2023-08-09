9a30f6c5f9957d4c0dd072e0d7af3a055485f226
IraMira333 <irinat333@gmail.com>
2023-08-09 15:35:09 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
 import ReactDOM from 'react-dom/client';
-import App from 'components/App';
+import App from 'App';
 import './index.css';
 import { Provider } from 'react-redux';
 import { store } from 'reduxThunk/store';
+import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <Provider store={store}>
-    <App />
-  </Provider>
+  <React.StrictMode>
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
+  </React.StrictMode>
 );
