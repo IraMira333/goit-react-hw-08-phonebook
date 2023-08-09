0e455a6e60e5df127b21cf8b1fe83011c77ef2bd
IraMira333 <irinat333@gmail.com>
2023-08-09 17:23:48 +0300

    Update ContactsPage.jsx

--- src/pages/ContactsPage.jsx
+++ src/pages/ContactsPage.jsx
@@ -1,3 +1,41 @@
+import ContactList from 'components/ContactList/ContactList';
+import Filter from 'components/Filter/Filter';
+import Spiner from 'components/Spiner/Spiner';
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import {
+  selectContacts,
+  selectError,
+  selectFilteredContacts,
+  selectIsLoading,
+} from 'reduxThunk/selectors';
+import * as contactsOperations from '../reduxThunk/operationsThunk';
+
 export default function ContactsPage() {
-  return <h2>ContactsPage</h2>;
+  const contacts = useSelector(selectContacts);
+  const filteredContacts = useSelector(selectFilteredContacts);
+  const isLoading = useSelector(selectIsLoading);
+  const isError = useSelector(selectError);
+  const showContacts = !isLoading && !isError && contacts.length > 0;
+  const showNoContacts = !isLoading && !isError && contacts.length === 0;
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(contactsOperations.fetchContacts());
+  }, [dispatch]);
+
+  return (
+    <>
+      <h2>Contacts</h2>
+      {isLoading && <Spiner />}
+      {isError && <p>{isError}</p>}{' '}
+      {showContacts && (
+        <>
+          <Filter />
+          <ContactList contacts={filteredContacts} />
+        </>
+      )}
+      {showNoContacts && <p>You don't have any contact yet</p>}
+    </>
+  );
 }
