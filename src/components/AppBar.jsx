a6c2e41cdbd94089f53c6f69373da5ad750228a9
IraMira333 <irinat333@gmail.com>
2023-08-09 17:23:37 +0300

    Create AppBar.jsx

---
+++ src/components/AppBar.jsx
@@ -0,0 +1,36 @@
+import { useSelector } from 'react-redux';
+import Navigation from './Navigation';
+//import UserMenu from './UserMenu';
+import AuthNav from './AuthNav';
+import { Suspense } from 'react';
+import { Outlet } from 'react-router-dom';
+import Spiner from './Spiner/Spiner';
+//import { authSelectors } from '../redux/auth';
+
+const styles = {
+  header: {
+    display: 'flex',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    borderBottom: '1px solid #2A363B',
+  },
+};
+
+export default function AppBar() {
+  //const isLoggedIn = useSelector(authSelectors.getIsLoggedIn);
+  return (
+    <>
+      <header style={styles.header}>
+        <Navigation />
+
+        <AuthNav />
+        {/* {isLoggedIn ? <UserMenu /> : <AuthNav />} */}
+      </header>
+      <main>
+        <Suspense fallback={<Spiner />}>
+          <Outlet />
+        </Suspense>
+      </main>
+    </>
+  );
+}
