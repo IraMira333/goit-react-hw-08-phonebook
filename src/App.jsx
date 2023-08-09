b540b83690604279b49b2d608d3f5ca3876fdb79
IraMira333 <irinat333@gmail.com>
2023-08-09 15:50:57 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import ContactList from './components/ContactList/ContactList';
 import Filter from './components/Filter/Filter';
 
 import Spiner from 'components/Spiner/Spiner';
+import { Switch, Route } from 'react-router-dom';
 import { useSelector, useDispatch } from 'react-redux';
 import {
   selectContacts,
@@ -13,6 +14,10 @@ import {
 import * as contactsOperations from './reduxThunk/operationsThunk';
 import { useEffect } from 'react';
 import Container from './components/Container/Container';
+import HomePage from 'pages/HomePage';
+import RegisterPage from 'pages/RegisterPage';
+import LoginPage from 'pages/LoginPage';
+import ContactsPage from 'pages/ContactsPage';
 
 export default function App() {
   const contacts = useSelector(selectContacts);
@@ -29,30 +34,30 @@ export default function App() {
   const showNoContacts = !isLoading && !isError && contacts.length === 0;
 
   return (
-    // <Container>
-    //   <AppBar />
-
-    //   <Switch>
-    //     <Route exact path="/" component={HomeView} />
-    //     <Route path="/register" component={RegisterView} />
-    //     <Route path="/login" component={LoginView} />
-    //     <Route path="/contacts" component={ContactsView} />
-    //<Route path="/addcontacts" component={addContactsView} />
-    //   </Switch>
-    // </Container>
     <Container>
-      <h2>Phonebook</h2>
-      <ContactForm />
-      <h2>Contacts</h2>
-      {isLoading && <Spiner />}
-      {isError && <p>{isError}</p>}
-      {showContacts && (
-        <>
-          <Filter />
-          <ContactList contacts={filteredContacts} />
-        </>
-      )}
-      {showNoContacts && <p>You don't have any contact yet</p>}
+      {/* <AppBar /> */}
+
+      <Switch>
+        <Route exact path="/" component={<HomePage />} />
+        <Route path="/register" component={<RegisterPage />} />
+        <Route path="/login" component={<LoginPage />} />
+        <Route path="/contacts" component={<ContactsPage />} />
+        <Route path="/addcontacts" component={<AddContactsPage />} />
+      </Switch>
     </Container>
+    // <Container>
+    //   <h2>Phonebook</h2>
+    //   <ContactForm />
+    //   <h2>Contacts</h2>
+    //   {isLoading && <Spiner />}
+    //   {isError && <p>{isError}</p>}
+    //   {showContacts && (
+    //     <>
+    //       <Filter />
+    //       <ContactList contacts={filteredContacts} />
+    //     </>
+    //   )}
+    //   {showNoContacts && <p>You don't have any contact yet</p>}
+    // </Container>
   );
 }
