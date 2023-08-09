d6db3c39e02b18444c4ec812265fbdfd06139d73
IraMira333 <irinat333@gmail.com>
2023-08-09 17:23:35 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,50 +1,52 @@
-import ContactForm from './components/ContactForm/ContactForm';
-import ContactList from './components/ContactList/ContactList';
-import Filter from './components/Filter/Filter';
+// import ContactForm from './components/ContactForm/ContactForm';
+// import ContactList from './components/ContactList/ContactList';
+// import Filter from './components/Filter/Filter';
 
-import Spiner from 'components/Spiner/Spiner';
-import { Switch, Route } from 'react-router-dom';
-import { useSelector, useDispatch } from 'react-redux';
-import {
-  selectContacts,
-  selectError,
-  selectFilteredContacts,
-  selectIsLoading,
-} from 'reduxThunk/selectors';
-import * as contactsOperations from './reduxThunk/operationsThunk';
-import { useEffect } from 'react';
+// import Spiner from 'components/Spiner/Spiner';
+import { Navigate, Route, Routes } from 'react-router-dom';
+// import { useSelector, useDispatch } from 'react-redux';
+// import {
+//   selectContacts,
+//   selectError,
+//   selectFilteredContacts,
+//   selectIsLoading,
+// } from 'reduxThunk/selectors';
+// import * as contactsOperations from './reduxThunk/operationsThunk';
+// import { useEffect } from 'react';
 import Container from './components/Container/Container';
 import HomePage from 'pages/HomePage';
 import RegisterPage from 'pages/RegisterPage';
 import LoginPage from 'pages/LoginPage';
 import ContactsPage from 'pages/ContactsPage';
+import AddContactsPage from 'pages/AddContactsPage';
+import AppBar from 'components/AppBar';
 
 export default function App() {
-  const contacts = useSelector(selectContacts);
-  const filteredContacts = useSelector(selectFilteredContacts);
-  const isLoading = useSelector(selectIsLoading);
-  const isError = useSelector(selectError);
+  // const contacts = useSelector(selectContacts);
+  // const filteredContacts = useSelector(selectFilteredContacts);
+  // const isLoading = useSelector(selectIsLoading);
+  // const isError = useSelector(selectError);
 
-  const dispatch = useDispatch();
+  // const dispatch = useDispatch();
 
-  useEffect(() => {
-    dispatch(contactsOperations.fetchContacts());
-  }, [dispatch]);
-  const showContacts = !isLoading && !isError && contacts.length > 0;
-  const showNoContacts = !isLoading && !isError && contacts.length === 0;
+  // useEffect(() => {
+  //   dispatch(contactsOperations.fetchContacts());
+  // }, [dispatch]);
+  // const showContacts = !isLoading && !isError && contacts.length > 0;
+  // const showNoContacts = !isLoading && !isError && contacts.length === 0;
 
   return (
-    <Container>
-      {/* <AppBar /> */}
+    <Routes>
+      <Route path="/" element={<AppBar />}>
+        <Route index element={<HomePage />} />
+        <Route path="register" element={<RegisterPage />} />
+        <Route path="login" element={<LoginPage />} />
+        <Route path="contacts" element={<ContactsPage />} />
+        <Route path="addcontacts" element={<AddContactsPage />} />
+        <Route path="*" element={<Navigate to="/" />} />
+      </Route>
+    </Routes>
 
-      <Switch>
-        <Route exact path="/" component={<HomePage />} />
-        <Route path="/register" component={<RegisterPage />} />
-        <Route path="/login" component={<LoginPage />} />
-        <Route path="/contacts" component={<ContactsPage />} />
-        <Route path="/addcontacts" component={<AddContactsPage />} />
-      </Switch>
-    </Container>
     // <Container>
     //   <h2>Phonebook</h2>
     //   <ContactForm />
