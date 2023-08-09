f190a611642e3277a8a9d2d6ed30400bb9d26b90
IraMira333 <irinat333@gmail.com>
2023-08-09 15:51:07 +0300

    Create RegisterPage.jsx

---
+++ src/pages/RegisterPage.jsx
@@ -0,0 +1,36 @@
+export default function RegisterPage() {
+  return (
+    <div>
+      <h1>Страница регистрации</h1>
+
+      {/* <form onSubmit={handleSubmit} style={styles.form} autoComplete="off">
+        <label style={styles.label}>
+          Имя
+          <input type="text" name="name" value={name} onChange={handleChange} />
+        </label>
+
+        <label style={styles.label}>
+          Почта
+          <input
+            type="email"
+            name="email"
+            value={email}
+            onChange={handleChange}
+          />
+        </label>
+
+        <label style={styles.label}>
+          Пароль
+          <input
+            type="password"
+            name="password"
+            value={password}
+            onChange={handleChange}
+          />
+        </label>
+
+        <button type="submit">Зарегистрироваться</button>
+      </form> */}
+    </div>
+  );
+}
