07ced8de9e3fcd9ef22862a0a75c0b5d01ab81b2
IraMira333 <irinat333@gmail.com>
2023-08-10 14:07:29 +0300

    Update LoginPage.jsx

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import { useDispatch } from 'react-redux';
-import { authOperations } from 'reduxThunk/auth';
+import { logIn } from 'reduxThunk/auth';
 
 const styles = {
   form: {
@@ -31,7 +31,7 @@ export default function LoginPage() {
 
   const handleSubmit = e => {
     e.preventDefault();
-    dispatch(authOperations.logIn({ email, password }));
+    dispatch(logIn({ email, password }));
     setEmail('');
     setPassword('');
   };
