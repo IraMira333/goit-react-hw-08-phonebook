c6b72bc851692cbbc72950445115b95da11a7590
IraMira333 <irinat333@gmail.com>
2023-08-10 23:31:18 +0300

    Update UserMenu.jsx

--- src/components/UserMenu/UserMenu.jsx
+++ src/components/UserMenu/UserMenu.jsx
@@ -1,20 +1,17 @@
 import { useDispatch, useSelector } from 'react-redux';
 
-//import defaultAvatar from './default-avatar.png';
 import styles from './UserMenu.module.css';
 import { authSelectors, logOut } from 'reduxThunk/auth';
 
 export default function UserMenu() {
   const dispatch = useDispatch();
   const name = useSelector(authSelectors.getUsername);
-  // const avatar = defaultAvatar;
 
   return (
     <div className={styles.container}>
-      {/* <img src={avatar} alt="" width="32" className={styles.avatar} /> */}
-      <span className={styles.name}>Добро пожаловать, {name}</span>
+      <span className={styles.name}>Hello, {name}</span>
       <button type="button" onClick={() => dispatch(logOut())}>
-        Выйти
+        LogOut
       </button>
     </div>
   );
