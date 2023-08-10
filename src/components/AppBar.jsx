3431d10d26cdba47f8b5477517add2f291e9806c
IraMira333 <irinat333@gmail.com>
2023-08-10 14:07:19 +0300

    Update AppBar.jsx

--- src/components/AppBar.jsx
+++ src/components/AppBar.jsx
@@ -1,11 +1,12 @@
-//import { useSelector } from 'react-redux';
+import { useSelector } from 'react-redux';
 import Navigation from './Navigation';
-//import UserMenu from './UserMenu';
-import AuthNav from './AuthNav';
+
 import { Suspense } from 'react';
 import { Outlet } from 'react-router-dom';
 import Spiner from './Spiner/Spiner';
-//import { authSelectors } from '../redux/auth';
+import { authSelectors } from 'reduxThunk/auth';
+import UserMenu from './UserMenu/UserMenu';
+import AuthNav from './AuthNav';
 
 const styles = {
   header: {
@@ -17,14 +18,13 @@ const styles = {
 };
 
 export default function AppBar() {
-  //const isLoggedIn = useSelector(authSelectors.getIsLoggedIn);
+  const isLoggedIn = useSelector(authSelectors.getIsLoggedIn);
   return (
     <>
       <header style={styles.header}>
         <Navigation />
 
-        <AuthNav />
-        {/* {isLoggedIn ? <UserMenu /> : <AuthNav />} */}
+        {isLoggedIn ? <UserMenu /> : <AuthNav />}
       </header>
       <main>
         <Suspense fallback={<Spiner />}>
