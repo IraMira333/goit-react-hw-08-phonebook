e5e0e9ca91feea3cfc2cfba63250bb0a0e1e5fc9
IraMira333 <irinat333@gmail.com>
2023-08-10 13:42:53 +0300

    Update RegisterPage.jsx

--- src/pages/RegisterPage.jsx
+++ src/pages/RegisterPage.jsx
@@ -1,9 +1,49 @@
+import { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { authOperations } from 'reduxThunk/auth';
+
+const styles = {
+  form: {
+    width: 320,
+  },
+  label: {
+    display: 'flex',
+    flexDirection: 'column',
+    marginBottom: 15,
+  },
+};
+
 export default function RegisterPage() {
+  const dispatch = useDispatch();
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleChange = ({ target: { name, value } }) => {
+    switch (name) {
+      case 'name':
+        return setName(value);
+      case 'email':
+        return setEmail(value);
+      case 'password':
+        return setPassword(value);
+      default:
+        return;
+    }
+  };
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    dispatch(authOperations.register({ name, email, password }));
+    setName('');
+    setEmail('');
+    setPassword('');
+  };
   return (
     <div>
       <h1>Страница регистрации</h1>
 
-      {/* <form onSubmit={handleSubmit} style={styles.form} autoComplete="off">
+      <form onSubmit={handleSubmit} style={styles.form} autoComplete="on">
         <label style={styles.label}>
           Имя
           <input type="text" name="name" value={name} onChange={handleChange} />
@@ -30,7 +70,7 @@ export default function RegisterPage() {
         </label>
 
         <button type="submit">Зарегистрироваться</button>
-      </form> */}
+      </form>
     </div>
   );
 }
