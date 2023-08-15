08c62948f4a531699cc5d9aa3da47cbfd1735ca9
IraMira333 <irinat333@gmail.com>
2023-08-15 12:24:12 +0300

    Update Navigation.jsx

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
 import { NavLink } from 'react-router-dom';
-import { authSelectors } from 'reduxThunk/auth';
+import { authSelectors } from 'redux/auth';
 
 const styles = {
   link: {
