4e512c7e0baf0ea3a84ea39ad31c7c7a2aba0884
IraMira333 <irinat333@gmail.com>
2023-08-15 22:54:54 +0300

    Update Navigation.jsx

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,46 +1,19 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
-import { NavLink } from 'react-router-dom';
 import { authSelectors } from 'redux/auth';
+import { StyledNavLink } from './TopBar/TopBar.styled';
 
-const styles = {
-  link: {
-    display: 'inline-block',
-    textDecoration: 'none',
-    padding: 12,
-    fontWeight: 700,
-    color: '#2A363B',
-  },
-  activeLink: {
-    color: '#E84A5F',
-  },
-};
 export default function Navigation() {
   const isLoggedIn = useSelector(authSelectors.selectIsLoggedIn);
   const privateInfo = (
     <>
-      <NavLink
-        to="/contacts"
-        style={styles.link}
-        activestyle={styles.activeLink}
-      >
-        Contacts
-      </NavLink>
-
-      <NavLink
-        to="/addcontacts"
-        style={styles.link}
-        activestyle={styles.activeLink}
-      >
-        Add Contact
-      </NavLink>
+      <StyledNavLink to="/contacts">Contacts</StyledNavLink>
+      <StyledNavLink to="/addcontacts">Add Contact</StyledNavLink>
     </>
   );
   return (
     <nav>
-      <NavLink to="/" style={styles.link} activestyle={styles.activeLink}>
-        HomePage
-      </NavLink>
+      <StyledNavLink to="/">HomePage</StyledNavLink>
 
       {isLoggedIn && privateInfo}
     </nav>
