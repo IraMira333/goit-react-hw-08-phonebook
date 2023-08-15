2257dde9de250c43c76b84e2d18db5e40734bcba
IraMira333 <irinat333@gmail.com>
2023-08-15 22:54:51 +0300

    Update AppBar.jsx

--- src/components/AppBar.jsx
+++ src/components/AppBar.jsx
@@ -4,25 +4,17 @@ import { authSelectors } from 'redux/auth';
 import UserMenu from './UserMenu/UserMenu';
 import AuthNav from './AuthNav';
 import Navigation from './Navigation';
-
-const styles = {
-  header: {
-    display: 'flex',
-    justifyContent: 'space-between',
-    alignItems: 'center',
-    borderBottom: '1px solid #2A363B',
-  },
-};
+import { StyledHeader } from './TopBar/TopBar.styled';
 
 export default function AppBar() {
   const isLoggedIn = useSelector(authSelectors.selectIsLoggedIn);
   return (
     <>
-      <header style={styles.header}>
+      <StyledHeader>
         <Navigation />
 
         {isLoggedIn ? <UserMenu /> : <AuthNav />}
-      </header>
+      </StyledHeader>
     </>
   );
 }
