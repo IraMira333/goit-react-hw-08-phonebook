4fd7a7d77aa408fb0d7583c8b653055c3b92e358
IraMira333 <irinat333@gmail.com>
2023-08-16 00:11:44 +0300

    Update TopBar.styled.js

--- src/components/TopBar/TopBar.styled.js
+++ src/components/TopBar/TopBar.styled.js
@@ -4,7 +4,7 @@ import styled from 'styled-components';
 export const StyledNavLink = styled(NavLink)`
   display: inline-block;
   text-decoration: none;
-  padding: 12px;
+  padding: 15px;
   font-weight: 700;
   color: #2a363b;
   transition: color cubic-bezier(0.25, 0.46, 0.45, 0.94);
@@ -18,6 +18,8 @@ export const StyledHeader = styled.header`
   display: flex;
   justify-content: space-between;
   align-items: center;
+
+  padding-right: 15px;
   border-bottom: 1px solid #2a363b;
   background-color: #b6c6ec;
   background-image: linear-gradient(
@@ -25,4 +27,16 @@ export const StyledHeader = styled.header`
     rgba(124, 24, 158, 0.4),
     rgba(124, 24, 158, 0.1)
   );
+`;
+export const StyledHelloUserDiv = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+export const StyledHelloUserSpan = styled.span`
+  font-weight: 700;
+  font-size: 18px;
+  color: #2a363b;
+  font-family: Roboto, sans-serif;
+  margin-right: 12px;
 `;
