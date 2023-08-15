2fe1cbf2fb995b897f7e3c168afb013dd75c478f
IraMira333 <irinat333@gmail.com>
2023-08-16 01:19:08 +0300

    Update TopBar.styled.js

--- src/components/TopBar/TopBar.styled.js
+++ src/components/TopBar/TopBar.styled.js
@@ -18,8 +18,8 @@ export const StyledHeader = styled.header`
   display: flex;
   justify-content: space-between;
   align-items: center;
-
-  padding-right: 15px;
+  flex: 0 0 auto;
+  /* padding-right: 15px; */
   border-bottom: 1px solid #2a363b;
   background-color: #b6c6ec;
   background-image: linear-gradient(
