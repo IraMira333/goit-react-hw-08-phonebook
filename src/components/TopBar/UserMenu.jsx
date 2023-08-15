7908367cec2abccec01355d275478e94b0395668
IraMira333 <irinat333@gmail.com>
2023-08-16 00:11:46 +0300

    Create UserMenu.jsx

---
+++ src/components/TopBar/UserMenu.jsx
@@ -0,0 +1,24 @@
+import { useDispatch, useSelector } from 'react-redux';
+import * as React from 'react';
+
+import { authSelectors, logOut } from 'redux/auth';
+import { ColorButton } from './UserMenu.styled';
+import { StyledHelloUserDiv, StyledHelloUserSpan } from './TopBar.styled';
+
+export default function UserMenu() {
+  const dispatch = useDispatch();
+  const name = useSelector(authSelectors.selectUsername);
+
+  return (
+    <StyledHelloUserDiv>
+      <StyledHelloUserSpan>Hello, {name}</StyledHelloUserSpan>
+      <ColorButton
+        variant="contained"
+        type="button"
+        onClick={() => dispatch(logOut())}
+      >
+        LogOut
+      </ColorButton>
+    </StyledHelloUserDiv>
+  );
+}
