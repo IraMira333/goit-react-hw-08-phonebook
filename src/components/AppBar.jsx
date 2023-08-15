80f1c535e3d370fd5fe46d79ccf2f4ac328a7994
IraMira333 <irinat333@gmail.com>
2023-08-15 14:55:21 +0300

    Update AppBar.jsx

--- src/components/AppBar.jsx
+++ src/components/AppBar.jsx
@@ -7,6 +7,11 @@ import Spiner from './Spiner/Spiner';
 import { authSelectors } from 'redux/auth';
 import UserMenu from './UserMenu/UserMenu';
 import AuthNav from './AuthNav';
+import Footer from './Footer';
+
+import CssBaseline from '@mui/material/CssBaseline';
+import Box from '@mui/material/Box';
+import Container from '@mui/material/Container';
 
 const styles = {
   header: {
@@ -21,16 +26,28 @@ export default function AppBar() {
   const isLoggedIn = useSelector(authSelectors.selectIsLoggedIn);
   return (
     <>
-      <header style={styles.header}>
-        <Navigation />
+      <Box
+        sx={{
+          display: 'flex',
+          flexDirection: 'column',
+          minHeight: '100vh',
+        }}
+      >
+        <header style={styles.header}>
+          <Navigation />
 
-        {isLoggedIn ? <UserMenu /> : <AuthNav />}
-      </header>
-      <main>
-        <Suspense fallback={<Spiner />}>
-          <Outlet />
-        </Suspense>
-      </main>
+          {isLoggedIn ? <UserMenu /> : <AuthNav />}
+        </header>
+
+        <CssBaseline />
+        <Container component="main">
+          <Suspense fallback={<Spiner />}>
+            <Outlet />
+          </Suspense>
+        </Container>
+
+        <Footer />
+      </Box>
     </>
   );
 }
