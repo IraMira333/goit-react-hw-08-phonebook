a30495f8681ef22ba4bffe0764e9c1be10461c9e
IraMira333 <irinat333@gmail.com>
2023-08-17 00:58:17 +0300

    Create support.js

---
+++ src/support/support.js
@@ -0,0 +1,8 @@
+const headerEl = document.body.querySelector('header');
+const footerEl = document.body.querySelector('footer');
+
+const headerHeight = headerEl.clientHeight;
+const footerHeight = footerEl.clientHeight;
+const pageHeight = document.documentElement.clientHeight;
+const minBoxHeight = pageHeight - headerHeight - footerHeight;
+export const minBoxHeightPx = `${minBoxHeight}px`;
