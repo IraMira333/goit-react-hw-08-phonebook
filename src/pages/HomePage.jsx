e18959b14f544ed8e6144fa0fce099ae51c57460
IraMira333 <irinat333@gmail.com>
2023-08-18 01:53:01 +0300

    Update HomePage.jsx

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,10 +1,17 @@
 import React from 'react';
 import Container from '@mui/material/Container';
 import { minBoxHeightPx } from 'support/support';
+import backgroundImage from 'support/PhoneBookHome.jpeg';
+import { Typography } from '@material-ui/core';
+import { StyledNavLink } from 'components/TopBar/TopBar.styled';
 
 const HomePage = () => (
   <Container
     sx={{
+      backgroundImage: `url(${backgroundImage})`,
+      backgroundRepeat: 'no-repeat',
+      backgroundSize: 'cover',
+      backgroundPosition: 'center',
       bgcolor: '#cfe8fc',
       marginLeft: '0',
       marginRight: '0',
@@ -12,7 +19,14 @@ const HomePage = () => (
       minHeight: minBoxHeightPx,
       textAlign: 'center',
     }}
-  ></Container>
+  >
+    <Typography variant="h4" gutterBottom>
+      <StyledNavLink to="/login" sx={{ fontSize: '24px' }}>
+        LogIn
+      </StyledNavLink>{' '}
+      to access your contacts.
+    </Typography>
+  </Container>
 );
 
 export default HomePage;
