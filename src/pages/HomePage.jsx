4f438113c8c8c107090772609e5e5283a40d01ae
IraMira333 <irinat333@gmail.com>
2023-08-17 22:43:18 +0300

    Update HomePage.jsx

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,6 +1,18 @@
 import React from 'react';
-import css from './HomePage.module.css';
+import Container from '@mui/material/Container';
+import { minBoxHeightPx } from 'support/support';
 
-const HomePage = () => <div className={css.container}></div>;
+const HomePage = () => (
+  <Container
+    sx={{
+      bgcolor: '#cfe8fc',
+      marginLeft: '0',
+      marginRight: '0',
+      paddingTop: '10px',
+      minHeight: minBoxHeightPx,
+      textAlign: 'center',
+    }}
+  ></Container>
+);
 
 export default HomePage;
