62f1b517f8b8b1e474a840bcbbec4a0916757049
IraMira333 <irinat333@gmail.com>
2023-08-17 22:43:14 +0300

    Update AddContactsPage.jsx

--- src/pages/AddContactsPage.jsx
+++ src/pages/AddContactsPage.jsx
@@ -1,9 +1,20 @@
 import ContactForm from 'components/ContactForm/ContactForm';
+import Container from '@mui/material/Container';
+import { minBoxHeightPx } from 'support/support';
 
 export default function AddContactsPage() {
   return (
-    <h2>
+    <Container
+      sx={{
+        bgcolor: '#cfe8fc',
+        marginLeft: '0',
+        marginRight: '0',
+        paddingTop: '10px',
+        minHeight: minBoxHeightPx,
+        textAlign: 'center',
+      }}
+    >
       <ContactForm />
-    </h2>
+    </Container>
   );
 }
