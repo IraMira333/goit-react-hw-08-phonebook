e14885e221f0d7676900de22413e0d97946ada8e
IraMira333 <irinat333@gmail.com>
2023-08-17 22:43:09 +0300

    Update ContactForm.jsx

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,9 +1,14 @@
 import { useDispatch, useSelector } from 'react-redux';
-import { useId } from 'react';
 
 import { addContact, selectContacts } from 'redux/contacts';
-import css from './ContactForm.module.css';
+
 import { useNavigate } from 'react-router-dom';
+
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+import Avatar from '@mui/material/Avatar';
+import ContactPhoneIcon from '@mui/icons-material/ContactPhone';
 
 export default function ContactForm() {
   const dispatch = useDispatch();
@@ -15,7 +20,7 @@ export default function ContactForm() {
     e.preventDefault();
     const name = e.currentTarget.name.value;
     const number = e.currentTarget.number.value;
-    console.log(name, number);
+
     const existingContact = findingDoubleName.find(
       el => el.name.toLocaleLowerCase() === name.toLocaleLowerCase()
     );
@@ -33,40 +38,83 @@ export default function ContactForm() {
     }
   };
 
-  const nameId = useId();
-  const numberId = useId();
-
   return (
-    <div className={css.formbox}>
-      <form className={css.form} onSubmit={onSubmit}>
-        <label className={css.label} htmlFor={nameId}>
-          Name
-        </label>
-        <input
-          className={css.input}
+    <Box
+      sx={{
+        width: '300px',
+        marginTop: 8,
+        border: '2px solid #4a148c',
+        borderRadius: '8px',
+        marginLeft: 'auto',
+        marginRight: 'auto',
+        bgcolor: '#f3e5f5',
+      }}
+    >
+      <Avatar
+        sx={{
+          m: 2,
+          marginLeft: 'auto',
+          marginRight: 'auto',
+          bgcolor: 'secondary.main',
+        }}
+      >
+        <ContactPhoneIcon />
+      </Avatar>
+      <Box
+        component="form"
+        onSubmit={onSubmit}
+        noValidate
+        sx={{
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+        }}
+      >
+        <TextField
           type="text"
           name="name"
-          id={nameId}
+          size="small"
+          id="outlined-basic"
+          label="Name"
           pattern="^[a-zA-Z\s]+$"
           title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
           required
+          variant="outlined"
+          sx={{
+            background: 'white',
+            borderRadius: '8px',
+            marginTop: '5px',
+          }}
         />
-        <label className={css.label} htmlFor={numberId}>
-          Number
-        </label>
-        <input
-          className={css.input}
+        <TextField
           type="tel"
           name="number"
-          id={numberId}
+          size="small"
+          id="outlined-basic"
+          label="Number"
           pattern="\+?\d{1,4}?[ .\-\s]?\(?\d{1,3}?\)?[ .\-\s]?\d{1,4}[ .\-\s]?\d{1,4}[ .\-\s]?\d{1,9}"
           title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
           required
+          variant="outlined"
+          sx={{
+            background: 'white',
+            borderRadius: '8px',
+            marginTop: '10px',
+          }}
         />
-        <button type="submit" className={css.formBtn}>
+
+        <Button
+          type="submit"
+          variant="contained"
+          sx={{
+            mt: 3,
+            mb: 2,
+            background: 'linear-gradient(to right bottom, #5275e9, #d08dd3)',
+          }}
+        >
           Add contact
-        </button>
-      </form>
-    </div>
+        </Button>
+      </Box>
+    </Box>
   );
 }
