a58b49b66689188b7c6808d861aa0b783a29f834
IraMira333 <irinat333@gmail.com>
2023-08-17 22:43:11 +0300

    Create Snack.jsx

---
+++ src/components/Snack/Snack.jsx
@@ -0,0 +1,12 @@
+import Snackbar from '@mui/material/Snackbar';
+import Alert from '@mui/material/Alert';
+
+export default function Snack({ isOpen, handleClose }) {
+  return (
+    <Snackbar open={isOpen} autoHideDuration={3000} onClose={handleClose}>
+      <Alert severity="success">
+        You have successfully added a contact to your list!
+      </Alert>
+    </Snackbar>
+  );
+}
