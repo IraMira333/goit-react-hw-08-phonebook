2831a21cf8e87101e8bf2e72117d431879ef20a7
IraMira333 <irinat333@gmail.com>
2023-08-18 11:31:28 +0300

    Update HomePage.jsx

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,32 +1,59 @@
 import React from 'react';
 import Container from '@mui/material/Container';
+import Link from '@mui/material/Link';
+import { useSelector } from 'react-redux';
+import { authSelectors } from 'redux/auth';
 import { minBoxHeightPx } from 'support/support';
 import backgroundImage from 'support/PhoneBookHome.jpeg';
-import { Typography } from '@material-ui/core';
-import { StyledNavLink } from 'components/TopBar/TopBar.styled';
+//import { Typography } from '@material-ui/core';
+//import { StyledNavLink } from 'components/TopBar/TopBar.styled';
 
-const HomePage = () => (
-  <Container
-    sx={{
-      backgroundImage: `url(${backgroundImage})`,
-      backgroundRepeat: 'no-repeat',
-      backgroundSize: 'cover',
-      backgroundPosition: 'center',
-      bgcolor: '#cfe8fc',
-      marginLeft: '0',
-      marginRight: '0',
-      paddingTop: '10px',
-      minHeight: minBoxHeightPx,
-      textAlign: 'center',
-    }}
-  >
-    <Typography variant="h4" gutterBottom>
-      <StyledNavLink to="/login" sx={{ fontSize: '24px' }}>
-        LogIn
-      </StyledNavLink>{' '}
-      to access your contacts.
-    </Typography>
-  </Container>
-);
-
-export default HomePage;
+export default function HomePage() {
+  const isLoggedIn = useSelector(authSelectors.selectIsLoggedIn);
+  const isUserName = useSelector(authSelectors.selectUsername);
+  return (
+    <Container
+      sx={{
+        backgroundImage: `url(${backgroundImage})`,
+        backgroundRepeat: 'no-repeat',
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+        bgcolor: '#cfe8fc',
+        marginLeft: '0',
+        marginRight: '0',
+        paddingTop: '10px',
+        minHeight: minBoxHeightPx,
+        textAlign: 'center',
+      }}
+    >
+      {!isLoggedIn ? (
+        <div>
+          <h1>
+            Please{' '}
+            <Link
+              href="https://iramira333.github.io/goit-react-hw-08-phonebook/register"
+              underline="hover"
+            >
+              Register
+            </Link>{' '}
+            or{' '}
+            <Link
+              href="https://iramira333.github.io/goit-react-hw-08-phonebook/login"
+              underline="hover"
+            >
+              Login
+            </Link>
+            .
+          </h1>
+        </div>
+      ) : (
+        <div>
+          <h1>Hello {isUserName?.name}.</h1>
+          <h2>
+            Would you like to see your <Link to="contacts"> contacts</Link>?
+          </h2>
+        </div>
+      )}
+    </Container>
+  );
+}
